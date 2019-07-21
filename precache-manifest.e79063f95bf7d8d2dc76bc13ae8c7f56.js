0b2fa2a2e6f7868526a3fa9c6d0b026b32db0ce0
Jeff Prestes <jeffprestes@gmail.com>
2019-07-21 18:18:41 -0300

    Updates

---
+++ precache-manifest.e79063f95bf7d8d2dc76bc13ae8c7f56.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32dd7e25db8af624819b07e630e498ba",
+    "url": "/tdc-palestras/index.html"
+  },
+  {
+    "revision": "507b62cd64540ef5f835",
+    "url": "/tdc-palestras/static/css/2.bbef705d.chunk.css"
+  },
+  {
+    "revision": "cf626c531d6c20b5a1d1",
+    "url": "/tdc-palestras/static/css/main.65117f9f.chunk.css"
+  },
+  {
+    "revision": "507b62cd64540ef5f835",
+    "url": "/tdc-palestras/static/js/2.9a72e1bc.chunk.js"
+  },
+  {
+    "revision": "cf626c531d6c20b5a1d1",
+    "url": "/tdc-palestras/static/js/main.f3f7d9fd.chunk.js"
+  },
+  {
+    "revision": "60b12fe14eb559333181",
+    "url": "/tdc-palestras/static/js/runtime~main.c9c856f4.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/tdc-palestras/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/tdc-palestras/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/tdc-palestras/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/tdc-palestras/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/tdc-palestras/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
